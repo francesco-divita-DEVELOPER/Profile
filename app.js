9d9cd61c84fb892aa89e63ac79e6dc3cef72f7ba
francesco-divita-DEVELOPER <107550282+francesco-divita-DEVELOPER@users.noreply.github.com>
2022-11-12 16:49:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express= require('express');
+
+const app=express();
+
+app.use(express.static("public"));
+
+app.get('/', (req, res)=>{
+
+    res.sendFile(__dirname + ("/home.html"));
+});
+
+app.listen('3000', ()=>{
+    console.log("Connesso")
+})
